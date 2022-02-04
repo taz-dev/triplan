a070a5ac09852e55addbc1bf7f501df95ee49c6b
taz <tazdev@naver.com>
2022-02-04 23:05:46 +0900

    feat: 현재 로그인한 사용자 정보 가져오기

---
+++ src/main/java/be/triplan/config/security/PrincipalDetail.java
@@ -0,0 +1,70 @@
+package be.triplan.config.security;
+
+import be.triplan.entity.Member;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import javax.persistence.ElementCollection;
+import javax.persistence.FetchType;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Getter
+public class PrincipalDetail implements UserDetails {
+
+    private Member member;
+
+    public PrincipalDetail(Member member) {
+        this.member = member;
+    }
+
+    @ElementCollection(fetch = FetchType.EAGER)
+    @Builder.Default
+    private List<String> roles = new ArrayList<>();
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return this.roles
+                .stream().map(SimpleGrantedAuthority::new)
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public String getPassword() {
+        return null;
+    }
+
+    @Override
+    public String getUsername() {
+        return null;
+    }
+
+    //계정이 만료되었는지 여부 (true : 만료안됨)
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    //계정이 잠겼는지 여부 (true : 잠기지않음)
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    //계정 패스워드가 만료되었는지 여부 (true : 만료안됨)
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    //계정이 사용가능한지 여부 (true : 활성화)
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
--- src/main/java/be/triplan/service/security/PrincipalDetailsService.java
+++ src/main/java/be/triplan/service/security/PrincipalDetailsService.java
@@ -1,7 +1,10 @@
 package be.triplan.service.security;
 
+import be.triplan.config.security.PrincipalDetail;
+import be.triplan.entity.Member;
 import be.triplan.exception.UserNotFoundException;
 import be.triplan.repository.MemberRepository;
+import be.triplan.service.MemberService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -9,16 +12,25 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
-public class CustomUserDetailsService implements UserDetailsService {
+public class PrincipalDetailsService implements UserDetailsService {
 
     private final MemberRepository memberRepository;
 
-    @Override
+/*    @Override
     public UserDetails loadUserByUsername(String userId) throws UsernameNotFoundException {
         return memberRepository.findById(Long.parseLong(userId))
                 .orElseThrow(UserNotFoundException::new);
+    }*/
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Member principal = memberRepository.findByNickname(username)
+                .orElseThrow(UserNotFoundException::new);
+
+        return new PrincipalDetail(principal);
     }
 }
