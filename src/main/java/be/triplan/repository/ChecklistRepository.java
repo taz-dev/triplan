049b0ce257b29e50845afbf15f42106f24564f17
taz <tazdev@naver.com>
2022-02-09 23:49:22 +0900

    chore: 체크리스트 서비스 수정

--- src/main/java/be/triplan/repository/ChecklistRepository.java
+++ src/main/java/be/triplan/repository/ChecklistRepository.java
@@ -4,5 +4,4 @@ import be.triplan.entity.Checklist;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ChecklistRepository extends JpaRepository<Checklist, Long> {
-
 }
